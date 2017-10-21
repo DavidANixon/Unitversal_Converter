21aa5d6f94f13f033d64d4ffa6d444232d5f4d43
Vincent Portelli <vinport16@gmail.com>
2017-10-21 17:27:51 -0400

    fixed some thingz

--- Unitversal_Converter/server.js
+++ Unitversal_Converter/server.js
@@ -7,7 +7,7 @@ var app = express();
 var port = process.env.PORT || 6969;
 
 //const client = new pg.Client(connectionString);
-client.connect();
+//client.connect();
 
 //pg.connect(connectionString, function(err, client, done) {
   //client.query('CREATE TABLE IF NOT EXISTS conversion(id PRIMARY KEY, date VARCHAR(60) not null, box0 INT, box1 INT, box2 INT, box3 INT, box4 INT, box5 INT, box6 INT, box7 INT, box8 INT, total INT, color VARCHAR(10), initials VARCHAR(3) )', function(err, result) {
@@ -23,8 +23,8 @@ app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/script.js", function(req, res){
-  res.sendFile(__dirname + "/script.js");
+app.get("/scripts.js", function(req, res){
+  res.sendFile(__dirname + "/scripts.js");
 });
 
 app.get("/stylesheet.css", function(req, res){
