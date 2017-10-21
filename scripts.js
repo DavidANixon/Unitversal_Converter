3ed200c5253403503de566cd6e58660c76831ba6
David Nixon <Dman42997@gmail.com>
2017-10-21 15:19:51 -0400

    Changed the stupid ass myFunction name

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-function myFunction() {
+function dropdownMenu() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
@@ -15,4 +15,4 @@ window.onclick = function(event) {
       }
     }
   }
-} 
+}
