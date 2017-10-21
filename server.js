556a2c6ec88f7f13aa1c54cc3d60c24e8c3ee565
Anna Sedlackova <axs1202@case.edu>
2017-10-21 14:25:26 -0400

    Changed port number

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+//made a comment
+
 var http = require('http');
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 5757;
+var port = process.env.PORT || 6969;
 
-http.createServer(app).listen(port, function () {
+http.createServer(app).listen(port, function (req,res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
   console.log("Express server listening on port " + port);
 });
