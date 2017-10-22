601c7c91df69575f079a5f6e83c12ea7103ec82e
Vincent Portelli <vinport16@gmail.com>
2017-10-22 08:45:32 -0400

    fixed scripts?

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ function dropdownMenu() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
-dat = {};
+var dat = {};
 
 ele = {};
 ele["length"] = 6;
@@ -23,6 +23,7 @@ car["energy"] = 7000;
 
 dat["car"] = car;
 
+console.log(dat);
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
@@ -41,12 +42,28 @@ window.onclick = function(event) {
 
 // When the user clicks the submit button
 function convert() {
+    firstThingToBeConverted = document.getElementById("firstThing").value;
+    secondThingToBeConverted = document.getElementById("secondThing").value;
 
+    var result;
+    console.log(firstThingToBeConverted);
+    console.log(unitsSelected);
+    if(document.getElementById("leftTextBox")===""){
+        result = (dat[secondThingToBeConverted][unitsSelected]* parseFloat(document.getElementById("rightTextBox").value)) /(dat[firstThingToBeConverted][unitsSelected]);
+        document.getElementById("leftTextBox").value = result;
+    }else {
+        result = (dat[firstThingToBeConverted][unitsSelected]* parseFloat(document.getElementById("leftTextBox").value)) /(dat[secondThingToBeConverted][unitsSelected]);
+        document.getElementById("rightTextBox").value = result;
+    }
+    console.log(result);
 }
 
+
 function setUnitsSelected() {
+
+
   unitsSelected = document.getElementById("units").value;
-  console.log(unitsSelected);
+  console.log(dat);
   var str;
   for (var key in dat) {
     if (dat.hasOwnProperty(key)) {
@@ -54,21 +71,11 @@ function setUnitsSelected() {
         str+= "<option value=\""+key+"\">"+key+"</option>";
       }
     }
-  }
+
+}
   document.getElementById("firstThing").innerHTML=str;
   document.getElementById("secondThing").innerHTML=str;
 }
-
-function setFirstThing() {
-  firstThingToBeConverted = document.getElementById("firstThing").value;
-  console.log(firstThingToBeConverted);
-}
-
-function setSecondThing(value){
-  secondThingToBeConverted = document.getElementById("secondThing").value;
-  console.log(firstThingToBeConverted);
-}
-
 
 socket.emit("data",0);
 
